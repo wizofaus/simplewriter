3e764c3f96d40109a698f6da5ec32890795762ee
Fourat Janabi <fouratj@gmail.com>
2017-06-21 12:34:09 -0300

    added flicker to text input screen

--- count.js
+++ count.js
@@ -1,13 +1,19 @@
 let $ = require('jquery');
+let contentWrapper = $('#contentWrapper');
 let wordCount = $('#word');
 let charCount = $('#char');
 
 const updateWordCount = setInterval(function () {
     let content = $('#content').val();
-    console.log(content.length);
-
     $(charCount).text(content.length);
     $(wordCount).text(content.split(' ').length);
-
-    console.log(content);
 }, 5000);
+
+window.addEventListener("keyup", function () {
+    $(contentWrapper).addClass('z-depth-2');
+    
+    setTimeout(function () {
+        $(contentWrapper).removeClass('z-depth-2');
+    }, 25);
+    
+});
