25282ec51f3284d7cb39e15b856d36b6dbc777dc
Fourat Janabi <fouratj@gmail.com>
2017-06-21 21:29:21 -0300

    Added info section to main page

--- count.js
+++ count.js
@@ -1,12 +1,19 @@
-// let $ = require('jquery');
 let contentWrapper = $('#contentWrapper');
 let wordCount = $('#word');
 let charCount = $('#char');
+let infoIcon = $('#info');
 
 const updateWordCount = setInterval(function () {
     let content = $('#content').val();
-    $(charCount).text(content.length);
-    $(wordCount).text(content.split(' ').length);
+
+    if (content && content.length > 0) { 
+        $(charCount).text(content.length);
+        $(wordCount).text(content.split(' ').length);
+        $('#char2').text(content.length);
+        $('#word2').text(content.split(' ').length);
+    }
+    
+    
 }, 5000);
 
 window.addEventListener("keyup", function () {
@@ -14,6 +21,18 @@ window.addEventListener("keyup", function () {
 
     setTimeout(function () {
         $(contentWrapper).removeClass('z-depth-2');
-    }, 25);
+    }, 25); 
+});
+
+info.addEventListener("mouseenter", function () {
+    $(contentWrapper).fadeOut("slow", function () {
+        $("#stats").fadeIn();
+    });
     
+});
+
+info.addEventListener("mouseleave", function () {
+    $("#stats").fadeOut("slow", function () {
+        $(contentWrapper).fadeIn();
+    });    
 });
